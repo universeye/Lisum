437fbad74da87f9c3335d6be886a0e9b35f6e04c
terrykuo <terry.501@hotmail.com>
2023-03-20 14:33:49 +0800

    Initial Set Up

---
+++ Lisum/SearchViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SearchViewController.swift
+//  Lisum
+//
+//  Created by Terry Kuo on 2023/3/20.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
