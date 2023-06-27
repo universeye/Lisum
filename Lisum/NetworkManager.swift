9d828a60e21ad2f3de4be4e1f710a1e686ba87c6
Terry Kuo <terrykuo501@gmail.com>
2023-06-27 14:50:40 +0800

    add URLComponents

--- Lisum/NetworkManager.swift
+++ Lisum/NetworkManager.swift
@@ -16,15 +16,24 @@ class NetworkManager {
     private init() {}
     
     func searchMusic(for searchTerm: String, offsetCount offset: Int) async throws -> SearchResult {
-        let replacedSpaceSearchTerm = searchTerm.replacingOccurrences(of: " ", with: "+")
-        let endpoint = baseURL + "search?term=\(replacedSpaceSearchTerm)&media=music&offset=\(offset)&limit=50"
+        let scheme = "https"
+        let host = "itunes.apple.com"
+        let path = "/search"
+        let queryItemTerm = URLQueryItem(name: "term", value: searchTerm)
+        let queryItemOffset = URLQueryItem(name: "offset", value: "\(offset)")
+        let queryItemLimit = URLQueryItem(name: "limit", value: "50")
+        let queryItemMedia = URLQueryItem(name: "media", value: "music")
         
-        guard let url = URL(string: endpoint) else { throw LisumError.invalidSearchTerm }
+        var urlComponents = URLComponents()
+        urlComponents.scheme = scheme
+        urlComponents.host = host
+        urlComponents.path = path
+        urlComponents.queryItems = [queryItemTerm, queryItemMedia, queryItemOffset, queryItemLimit]
+        guard let url = urlComponents.url else { throw LisumError.invalidSearchTerm }
         let (data, response) = try await URLSession.shared.data(from: url)
         guard (response as? HTTPURLResponse)?.statusCode == 200 else {
             print("error Response: \(response)")
             throw LisumError.invalidResponse
-            
         }
         
         let decoder = JSONDecoder()
